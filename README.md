9574e9663e7d3d3e312d138d664dd641ec0a093e
marcosfsousa <marcosfmsousa@gmail.com>
2021-05-29 20:28:26 +0200

    mobile design near to finalized status // improved desktop frameworks

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fed-final-project-marcosfsousa
 ReDi Munich Front End Development Course - Final Project
+
+This is my Final Project for the Front End Development Spring Course in ReDI Munich School. 
+
+The website is a interactive resume built with HTML, CSS and JavaScript.
---
+++ script.js
@@ -0,0 +1,14 @@
+// Mouseover tests //
+
+const experienceCard = document.querySelector('.text-experience')
+const educationCard = document.querySelector('.text-education')
+
+educationCard.addEventListener ("mouseover", (event) => {
+    educationCard.style.backgroundColor = 'red';
+});
+
+educationCard.addEventListener ("mouseout", (event) => {
+    educationCard.style.backgroundColor = '';
+});
+
+
