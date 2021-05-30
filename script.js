2ffec4b8369a207657674610932e26cd2d25aa44
marcosfsousa <marcosfmsousa@gmail.com>
2021-05-30 19:51:25 +0200

    overall mobile styles improved

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Mouseover tests //
+// Mouseover tests 
 
-const experienceCard = document.querySelector('.text-experience')
-const educationCard = document.querySelector('.text-education')
+// const experienceCard = document.querySelector('.text-experience')
+// const educationCard = document.querySelector('.text-education')
 
-educationCard.addEventListener ("mouseover", (event) => {
-    educationCard.style.backgroundColor = 'red';
-});
+// educationCard.addEventListener ("mouseover", (event) => {
+//     educationCard.style.backgroundColor = 'red';
+// });
 
-educationCard.addEventListener ("mouseout", (event) => {
-    educationCard.style.backgroundColor = '';
-});
+// educationCard.addEventListener ("mouseout", (event) => {
+//     educationCard.style.backgroundColor = '';
+// });
 
 
